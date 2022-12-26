b212d715933bcf58b60243ad0c1da2253126442b
TRAVIS <93493491+BABOAIO@users.noreply.github.com>
2022-12-26 18:22:55 +0900

    "Add Anim"

---
+++ Assets/02.Scripts/AshTraySpawner.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AshTraySpawner : MonoBehaviour
+{
+    public GameObject AshPrefab;
+    public float spawnRate = 1.0f;
+
+    private Transform target;
+    private float spawnTime = 0;
+    private float timeAfterSpawn;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeAfterSpawn = 0f;
+        target = FindObjectOfType<Player>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
